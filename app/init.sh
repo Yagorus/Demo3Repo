01fa0d2a0c064ff55e6caf33cd0c2a306081cdd3
Yagorus <yegor.sidenko@gmail.com>
2022-01-14 19:11:06 +0200

    Change sh script. Add echo vars. Update docker commands

--- app/init.sh
+++ app/init.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
-aws ecr get-login-password --region $REPOSITORY_REGION | docker login --username AWS --password-stdin $REGISTRY_ID.dkr.$REPOSITORY_REGION.amazonaws.com
-docker build $REPOSITORY_URL:$TAG
-docker push $REPOSITORY_URL:$TAG
+echo "REPOSITORY_REGION = ${REPOSITORY_REGION}"
+echo "REPOSITORY_URL:TAG = ${REPOSITORY_URL}:${TAG}"
+
+aws ecr get-login-password --region ${REPOSITORY_REGION} | docker login --username AWS --password-stdin ${REGISTRY_ID.dkr.$REPOSITORY_REGION.amazonaws.com}
+docker build ${REPOSITORY_URL}:${TAG} -f ./Dockerfile .
+docker push ${REPOSITORY_URL}:${TAG}
