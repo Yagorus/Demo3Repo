363bf81b930749306d2d0f9bcc388a447a337518
Yagorus <yegor.sidenko@gmail.com>
2022-01-16 19:01:31 +0200

    sadf

--- app/init.sh
+++ app/init.sh
@@ -4,7 +4,7 @@ echo "do REPOSITORY_URL:TAG = ${REPOSITORY_URL}:${TAG}"
 echo "do REGISTRY_ID = ${REGISTRY_ID}"
 
 aws ecr get-login-password --region $REPOSITORY_REGION | docker login --username AWS --password-stdin $REGISTRY_ID.dkr.ecr.$REPOSITORY_REGION.amazonaws.com
-docker build $REPOSITORY_URL:$TAG .
+docker build -t $REPOSITORY_URL:$TAG /home/ubuntu/Demo3Repo/app/
 docker push $REPOSITORY_URL:$TAG
 
 #aws ecr get-login-password --region eu-central-1 | docker login --username AWS --password-stdin 367668710117.dkr.ecr.eu-central-1.amazonaws.com
