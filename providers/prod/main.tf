03d135d7f175d63d851f665bdf49009f355b32ae
Yagorus <yegor.sidenko@gmail.com>
2022-01-17 09:28:36 +0200

    asdf

--- providers/prod/main.tf
+++ providers/prod/main.tf
@@ -28,7 +28,7 @@ module "ecs-cluster" {
     source = "../../modules//cluster"
     aws_region = var.aws_region
     aws_profile = var.aws_profile
-    remote_state_bucket = var.bucket_name
+    #remote_state_bucket = var.bucket_name
     environment = var.environment
     app_name = var.app_name
     image_tag = var.image_tag
