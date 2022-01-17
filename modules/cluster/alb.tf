3690317acac2974864a2aa123ee32eba24066f40
Yagorus <yegor.sidenko@gmail.com>
2022-01-17 08:18:23 +0200

    asdf

--- modules/cluster/alb.tf
+++ modules/cluster/alb.tf
@@ -2,7 +2,7 @@ resource "aws_alb" "application_load_balancer" {
   name               = "${var.app_name}-alb"
   internal           = false
   load_balancer_type = "application"
-  subnets            = aws_subnet.private.*.id
+  subnets            = aws_subnet.public.*.id
   security_groups    = [aws_security_group.load_balancer_security_group.id]
 
   tags = {
