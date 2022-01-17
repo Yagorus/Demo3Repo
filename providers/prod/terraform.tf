3757ca498238cc7a983bb7fdfb586ca3be825ff4
Yagorus <yegor.sidenko@gmail.com>
2022-01-17 09:04:10 +0200

    asdf

--- providers/prod/terraform.tf
+++ providers/prod/terraform.tf
@@ -1,6 +1,17 @@
-
 provider "aws" {
-    region      = var.aws_region
-    profile     = var.aws_profile
-    shared_credentials_file = var.aws_profile_access_key_path
+    region = var.aws_region
+    profile = var.aws_profile
 }
+
+terraform {
+  // backend "s3" {
+  //   encrypt = true
+  //   bucket  = "flaskapp-dev-eu-west-1"
+  //   region  = "eu-west-1"
+  //   key     = "state"
+  // }
+  required_providers {
+    aws = {
+      version = "~> 3.35"
+    }
+  }
