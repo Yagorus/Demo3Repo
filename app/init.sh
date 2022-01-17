106b2d834c765a5b42f7581f2652453599264db4
Yagorus <yegor.sidenko@gmail.com>
2022-01-17 11:00:51 +0200

    asdf

--- app/init.sh
+++ app/init.sh
@@ -5,9 +5,7 @@ echo "do REGISTRY_ID = ${REGISTRY_ID}"
 
 aws ecr get-login-password --region $REPOSITORY_REGION | docker login --username AWS --password-stdin $REGISTRY_ID.dkr.ecr.$REPOSITORY_REGION.amazonaws.com
 docker build -t $REPOSITORY_URL:$TAG /home/ubuntu/Demo3Repo/app/
-aws ecr batch-delete-image \
-     --repository-name ecr \
-     --image-ids imageTag=$TAG
+
 docker push $REPOSITORY_URL:$TAG
 
 #aws ecr get-login-password --region eu-central-1 | docker login --username AWS --password-stdin 367668710117.dkr.ecr.eu-central-1.amazonaws.com
